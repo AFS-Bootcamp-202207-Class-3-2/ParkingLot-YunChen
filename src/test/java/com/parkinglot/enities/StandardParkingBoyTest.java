1f4186f9b92a3b0e93e265fc648b1ed6bdeb2c69
YunChen <1183728554@qq.com>
2022-07-23 01:33:48 +0800

    test:should_return_right_tickets_of_two_car_when_fetch_each_car_given_two_ticket passed

--- src/test/java/com/parkinglot/enities/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/enities/StandardParkingBoyTest.java
@@ -11,7 +11,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StandardParkingBoyTest {
-
+    @Test
+    void should_parking_first_parking_lot_when_have_two_parking_lot_given_a_car_and_standard_parking_boy() throws UnRecognizedException, UnAvailablePositionException {
+        //given
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(new ParkingLot(1), new ParkingLot());
+        //when
+        Car car = new Car("测试的车牌定制化license");
+        Ticket ticket = standardParkingBoy.park(car);
+        //then
+        Car fetchCar = standardParkingBoy.getParkingLots().get(0).fetch(ticket);
+        assertThat(fetchCar).isEqualTo(car);
+    }
     @Test
     void should_parking_lot_first_full_and_parking_second_parking_log_second_when_parking_given_two_parking_lot() throws UnRecognizedException, UnAvailablePositionException {
         //given
@@ -23,6 +33,21 @@ public class StandardParkingBoyTest {
         //then
         Car fetchCar = standardParkingBoy.getParkingLots().get(1).fetch(ticket);
         assertThat(fetchCar).isEqualTo(car);
+    }
+
+    @Test
+    void should_return_right_tickets_of_two_car_when_fetch_each_car_given_two_ticket()throws Exception {
+        //given
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(new ParkingLot(1), new ParkingLot());
+        //when
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Ticket firstTicket = standardParkingBoy.park(firstCar);
+        Ticket secondTicket = standardParkingBoy.park(secondCar);
+        Car fetchSecondCar = standardParkingBoy.fetch(secondTicket);
+        Car fetchFirstCar = standardParkingBoy.fetch(firstTicket);
+        assertThat(firstCar).isEqualTo(fetchFirstCar);
+        assertThat(fetchSecondCar).isEqualTo(secondCar);
     }
 
 }
